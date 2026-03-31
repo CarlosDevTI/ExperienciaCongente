cb1e1a5afb867dd43e08aac05569a80f3716fcc3
CarlosDevTI <analistati.ingenieria@gmail.com>
2026-03-31 16:00:49 -0500

    fix: Solucion loop del admin luego de ingresar credenciales

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -1,12 +1,12 @@
 from .settings_common import *  # noqa: F403,F401
 from .settings_common import env, env_bool
 
-DEBUG = False
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+DEBUG = env_bool('DJANGO_DEBUG', default=False)
+CSRF_COOKIE_SECURE = env_bool('CSRF_COOKIE_SECURE', default=True)
+SESSION_COOKIE_SECURE = env_bool('SESSION_COOKIE_SECURE', default=True)
 SECURE_HSTS_SECONDS = int(env('SECURE_HSTS_SECONDS', 60 * 60 * 24 * 30))
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env_bool('SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
 SECURE_HSTS_PRELOAD = env_bool('SECURE_HSTS_PRELOAD', default=True)
 SECURE_SSL_REDIRECT = env_bool('SECURE_SSL_REDIRECT', default=True)
 SECURE_REFERRER_POLICY = env('SECURE_REFERRER_POLICY', 'strict-origin-when-cross-origin')
-SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_CONTENT_TYPE_NOSNIFF = env_bool('SECURE_CONTENT_TYPE_NOSNIFF', default=True)
